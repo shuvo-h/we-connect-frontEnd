c5dcdc646649de36b8eed0b7cbdc3a24f290519a
hamunna <hamunna@gmail.com>
2022-01-02 16:16:06 +0600

    Firebase Initialized

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAGfl1anNtbL70vM5htWgf5-4DdI32Jz38",
+	authDomain: "we-connect-ea837.firebaseapp.com",
+	projectId: "we-connect-ea837",
+	storageBucket: "we-connect-ea837.appspot.com",
+	messagingSenderId: "857174852647",
+	appId: "1:857174852647:web:fa06fb2033202120e1fa02"
+
+	// apiKey: process.env.REACT_APP_API_KEY,
+	// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	// projectId: process.env.REACT_APP_PROJECT_ID,
+	// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	// appId: process.env.REACT_APP_APP_ID
+
+};
+
+export default firebaseConfig;
