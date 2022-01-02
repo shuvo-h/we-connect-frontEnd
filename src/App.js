91ed640a8374746a63dce018aad30bee3cdebbea
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-01-02 16:49:18 +0600

    contact-page

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import Services from "./components/Services/Services/Services";
 // import Admin from './components/Dashboard/Admin/Admin/Admin';
 
 import AllUsers from "./components/AllUsers/AllUsers";
-
+import ContactUs from "./components/ContactUs/ContactUs";
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/users" element={<AllUsers></AllUsers>}></Route>
-          <Route path="/services" element={<Services></Services>}></Route>
-          <Route path="/home" element={<Home></Home>}></Route>
+    <Router>
+      <Routes>
+        <Route path="/users" element={<AllUsers></AllUsers>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/contact" element={<ContactUs></ContactUs>}></Route>
         <Route path="/" element={<Home></Home>}></Route>
-        
-          {/* <Route path="/adminDashboard" element={<Admin />}></Route> */}
-        </Routes>
-      </Router>
+
+        {/* <Route path="/adminDashboard" element={<Admin />}></Route> */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
