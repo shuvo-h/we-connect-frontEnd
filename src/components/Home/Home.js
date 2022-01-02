f5aa4aa078b0c94e4fb06697598d78f47fe4c18a
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-01-02 13:56:03 +0600

    footer added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
 import Header from "./Header/Header";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <Footer />
     </div>
   );
 };
