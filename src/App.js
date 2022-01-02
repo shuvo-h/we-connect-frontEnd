27db1aa8bd030ef0d346713ad8e65c599d7c2bf7
hamunna <hamunna@gmail.com>
2022-01-02 15:16:40 +0600

    munna first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Services from "./components/Services/Services/Services";
+// import Admin from './components/Dashboard/Admin/Admin/Admin';
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
         <Routes>
           <Route path="/services" element={<Services></Services>}></Route>
           <Route path="/home" element={<Home></Home>}></Route>
-          <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        
+          {/* <Route path="/adminDashboard" element={<Admin />}></Route> */}
         </Routes>
       </Router>
   );
