2b2b497eeb9703000a3f28f368a004a3885c47b7
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-01-03 00:29:08 +0600

    user and service highlight added

--- src/components/Home/ServiceHeighlight/SingleServiceHighLight.js
+++ src/components/Home/ServiceHeighlight/SingleServiceHighLight.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const SingleServiceHighLight = ({singleService}) => {
-    console.log(singleService);
-    return (
-        <div>
-            <h2>SingleServiceHighLight</h2>
-            {singleService.service_catogory}
+const SingleServiceHighLight = ({ singleService }) => {
+  const { service_banner, service_details, service_used, service_catogory } =
+    singleService;
+  console.log(singleService);
+  return (
+    <div className="col mb-2">
+      <div class="card">
+        <img src={service_banner} class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{service_catogory}</h5>
+          <p class="card-text">{service_details}</p>
+          <p>{service_used}</p>
+          <Link to="/all" class="btn btn-primary">
+            All {service_catogory}
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SingleServiceHighLight;
