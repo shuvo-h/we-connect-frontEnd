2fe473e129712f0be24e3c09a2e6b4fcd08cacc5
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-01-03 22:30:38 +0600

    dashboard home modified

--- src/components/DashboardComponents/DashboardHome/DashboardHome.js
+++ src/components/DashboardComponents/DashboardHome/DashboardHome.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const DashboardHome = () => {
-    return (
-        <div>
-            <h1>welcome 2 dashboard</h1>
+  const { user } = useAuth();
+  return (
+    <div className="text-center ">
+      <h1 className="py-4">Welcome To Dashboard</h1>
+      <div className="d-flex justify-content-center ">
+        <div className="text-center bg-danger p-3 rounded-3">
+          <p className="my-1">{user.displayName}</p>
+
+          <p className="my-1 container">{user.email}</p>
         </div>
-    );
+      </div>
+      <div className="">
+        {" "}
+        <img
+          className="home-style "
+          src="https://i.ibb.co/T09r0Zd/site-stats-concept-illustration-114360-1509-removebg-preview.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardHome;
--- src/components/Services/Services/Services.js
+++ src/components/Services/Services/Services.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { fetchServiceData } from "../../../redux/slices/serviceSlice";
+import Footer from "../../Shared/Footer/Footer";
+import Navigation from "../../Shared/Navigation/Navigation";
 import Service from "../Service/Service";
 
 const Services = () => {
@@ -12,12 +14,14 @@ const Services = () => {
   const services = useSelector((state) => state?.service?.allService[0]);
   return (
     <div>
+      <Navigation />
       <h2 className="fw-bold text-center my-4">Services we provide</h2>
       <div className="row container mx-auto ">
         {services?.map((service) => (
           <Service service={service} key={service._id}></Service>
         ))}
       </div>
+      <Footer />
     </div>
   );
 };
