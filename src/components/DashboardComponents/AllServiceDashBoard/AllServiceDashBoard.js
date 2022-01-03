d9a7e3c160ee5394529c754412c4360dd5c9254a
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-01-03 16:25:53 +0600

    add service modal

--- src/components/DashboardComponents/AllServiceDashBoard/AllServiceDashBoard.js
+++ src/components/DashboardComponents/AllServiceDashBoard/AllServiceDashBoard.js
@@ -1,24 +1,28 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { fetchServiceData } from '../../../redux/slices/serviceSlice';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { fetchServiceData } from "../../../redux/slices/serviceSlice";
+import AddSerevicesModal from "./AddSerevicesModal";
 
 const AllServiceDashBoard = () => {
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        dispatch(fetchServiceData())
-    },[])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchServiceData());
+  }, []);
 
-    const services = useSelector(state=>state?.service?.allService[0]);
-console.log(services);
-    return (
-        <div>
-            AllServiceDashBoard
-            {
-                services?.map(service=><li>{service.used} <img src={service.service_banner} alt="" /> </li>)
-            }
-        </div>
-    );
+  const services = useSelector((state) => state?.service?.allService[0]);
+  console.log(services);
+  return (
+    <div>
+      <AddSerevicesModal />
+
+      {services?.map((service) => (
+        <li>
+          {service.used} <img src={service.service_banner} alt="" />{" "}
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default AllServiceDashBoard;
