e393866aef9cc37b2bcf44a6be710fa1d4ebf450
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-01-04 22:43:30 +0600

    some style added

--- src/components/ProviverServiceCtg/CategoryServices.js
+++ src/components/ProviverServiceCtg/CategoryServices.js
@@ -3,11 +3,18 @@ import { Link } from "react-router-dom";
 
 const CategoryServices = ({ ctgUser }) => {
   const { img, name, _id } = ctgUser;
-  
+
   return (
     <div className="col-md-4 col-12 mb-4 rounded-3">
       <div class="card ">
-        <img src={img} class="card-img-top" alt="..." />
+        <img
+          style={{
+            height: "220px",
+          }}
+          src={img}
+          class="card-img-top"
+          alt="..."
+        />
         <div class="card-body">
           <h5 class="card-title">{name}</h5>
           <Link to={`/users/user/${_id}`} class="btn btn-primary">
