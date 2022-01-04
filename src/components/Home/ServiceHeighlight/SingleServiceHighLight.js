36ac9f96565160855383a9219fb26c258d3b6a7c
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-01-04 22:31:27 +0600

    some style added

--- src/components/Home/ServiceHeighlight/SingleServiceHighLight.js
+++ src/components/Home/ServiceHeighlight/SingleServiceHighLight.js
@@ -2,16 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const SingleServiceHighLight = ({ singleService }) => {
-  const { service_banner, service_details, service_used, service_catogory,_id } =
-    singleService;
-    
+  const {
+    service_banner,
+    service_details,
+    service_used,
+    service_catogory,
+    _id,
+  } = singleService;
+
+  const newServiceDetails = service_details.slice(0, 100);
+
   return (
     <div className="col mb-2">
       <div class="card">
-        <img src={service_banner} class="card-img-top" alt="..." />
+        <img
+          style={{
+            height: "200px",
+          }}
+          src={service_banner}
+          class="card-img-top "
+          alt="..."
+        />
         <div class="card-body">
           <h5 class="card-title">{service_catogory}</h5>
-          <p class="card-text">{service_details}</p>
+          <p class="card-text">{newServiceDetails}</p>
           <p>{service_used}</p>
           <Link to={`/services/${service_catogory}`} class="btn btn-primary">
             All {service_catogory}
--- src/components/Services/Service/Service.js
+++ src/components/Services/Service/Service.js
@@ -4,16 +4,27 @@ import { Link } from "react-router-dom";
 const Service = ({ service }) => {
   const { service_banner, service_catogory, service_details, service_used } =
     service;
-    
+
+  const newServiceDetails = service_details.slice(0, 150);
+
   return (
     <div className="col-4 mb-4 mt-3">
       <div class="card">
-        <img src={service_banner} class="card-img-top" alt="..." />
+        <img
+          style={{
+            height: "220px",
+          }}
+          src={service_banner}
+          class="card-img-top"
+          alt="..."
+        />
         <div class="card-body">
           <h5 class="card-title">{service_catogory}</h5>
-          <p class="card-text">{service_details}</p>
+          <p class="card-text">{newServiceDetails}</p>
           <p>{service_used}</p>
-          <Link to={`/services/${service_catogory}`} class="btn btn-primary">All {service_catogory}</Link>
+          <Link to={`/services/${service_catogory}`} class="btn btn-primary">
+            All {service_catogory}
+          </Link>
         </div>
       </div>
     </div>
@@ -21,4 +32,3 @@ const Service = ({ service }) => {
 };
 
 export default Service;
-
