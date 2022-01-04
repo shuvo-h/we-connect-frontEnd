246a5f10c21b9296aa064fb70ce4506e637fd089
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-01-04 23:05:08 +0600

    some style added

--- src/components/ProviverServiceCtg/ProviverServiceCtg.js
+++ src/components/ProviverServiceCtg/ProviverServiceCtg.js
@@ -5,8 +5,8 @@ import { fetchUsersCategory } from "../../redux/slices/usersSlice";
 import Footer from "../Shared/Footer/Footer";
 import Navigation from "../Shared/Navigation/Navigation";
 import CategoryServices from "./CategoryServices";
-import Navigation from "../Shared/Navigation/Navigation";
-import Footer from "../Shared/Footer/Footer";
+// import Navigation from "../Shared/Navigation/Navigation";
+// import Footer from "../Shared/Footer/Footer";
 
 const ProviverServiceCtg = () => {
   const { categoryName } = useParams();
@@ -19,9 +19,8 @@ const ProviverServiceCtg = () => {
   );
 
   return (
-
     <>
-      <Navigation />{" "}
+      <Navigation />
       <div className="container">
         <h3 className="text-center my-5 fw-bold">
           These people gives this category services{" "}
@@ -37,15 +36,10 @@ const ProviverServiceCtg = () => {
           </div>
         ) : (
           <div className="d-flex justify-content-center my-5">
-
             <div class="spinner-border text-primary" role="status">
               <span class="visually-hidden">Loading...</span>
             </div>
           </div>
-
-      }
-      <Footer></Footer>
-    </div>
         )}
       </div>
       <Footer />
